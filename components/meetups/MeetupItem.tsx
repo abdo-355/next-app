85fd5d87d87363fbb9f242ce025b37a3b3ef2055
abdo-355 <abdoluxor366@gmail.com>
2023-03-28 17:24:53 +0200

    implement programatic navigation to the meetup items

--- components/meetups/MeetupItem.tsx
+++ components/meetups/MeetupItem.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
+import { useRouter } from 'next/router';
+
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
@@ -8,7 +11,13 @@ export interface IMeetupItem {
   address: string;
 }
 
-const MeetupItem: React.FC<IMeetupItem> = ({ image, title, address }) => {
+const MeetupItem: React.FC<IMeetupItem> = ({ id, image, title, address }) => {
+  const router = useRouter();
+
+  const showDetailsHandler = () => {
+    router.push("/" + id)
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -20,7 +29,7 @@ const MeetupItem: React.FC<IMeetupItem> = ({ image, title, address }) => {
           <address>{address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
